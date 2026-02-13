f6a60a5d296d4039bf9ef5e9c34862974f289790
Simjrn <simonjenni374@gmail.com>
2026-02-13 18:04:07 +0000

    Refactor substring function and update test cases
    Removed debug prints and updated test substrings.

---
+++ substring.rb
@@ -0,0 +1,15 @@
+def substring(word, substrings)
+    results = {}
+    substrings.each do |item|
+        if word.include?(item)
+            if results.has_key?(item)
+                results[item] += 1
+            else
+                results[item] = 1
+            end
+        end
+    end
+    print results
+end
+substrings = ["low", "eat", "have", "hello", "l", "what", "how", "so"]
+substring("hello, how are you?", substrings)
